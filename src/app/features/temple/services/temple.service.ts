52c95938a0da3522fa90ffccc4ecaa91a0080e47
Sesha932000 <sesha932000@gmail.com>
2025-08-01 13:16:45 +0530

    finalCommit

--- src/app/features/temple/services/temple.service.ts
+++ src/app/features/temple/services/temple.service.ts
@@ -19,7 +19,7 @@ export class TempleService {
       name: 'Kabaleeshwar Temple',
       location: 'Chennai',
       description: 'Kapaleeswarar Temple stands as a testament to Tamil Nadus profound religious and cultural heritage. This ancient shrine, dedicated to Lord Shiva as Kapaleeswar and Goddess Shakthi (commonly referred to as Karpagambal), exudes reverence and mystique that has attracted devotees and tourists for centuries alike.',
-      imageUrl: 'assets/images/icons/card_images/Carousel image.png',
+      imageUrl: 'assets/images/icons/card_images/chennai.png',
       promoted: true
     },
     {
@@ -27,63 +27,63 @@ export class TempleService {
       name: 'Somnath Temple',
       location: 'Gujarat',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl: 'assets/images/icons/card_images/Image.png'
+      imageUrl: 'assets/images/icons/card_images/gujarat.jpg'
     },
     {
       id: '3',
       name: 'Golden Temple',
       location: 'Amritsar',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl:'assets/images/icons/card_images/Image (1).png'
+      imageUrl:'assets/images/icons/card_images/amristar.jpg'
     },
     {
       id: '4',
       name: 'Meenakshi Temple',
       location: 'Madurai',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl: 'assets/images/icons/card_images/Image (2).png'
+      imageUrl: 'assets/images/icons/card_images/madurai.jpg'
     },
     {
       id: '5',
       name: 'Kashi Temple',
       location: 'Varanasi',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl: 'assets/images/icons/card_images/Image (3).png'
+      imageUrl: 'assets/images/icons/card_images/varanasi.jpg'
     },
     {
       id: '6',
       name: 'Jagannath Temple',
       location: 'Puri',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl: 'assets/images/icons/card_images/Image (4).png'
+      imageUrl: 'assets/images/icons/card_images/puri.jpg'
     },
     {
       id: '7',
       name: 'Vaishno Devi',
       location: 'Katra',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl: 'assets/images/icons/card_images/Image (5).png'
+      imageUrl: 'assets/images/icons/card_images/katra.jpg'
     },
     {
       id: '8',
       name: 'Akshardham Temple',
       location: 'New Delhi',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl: 'assets/images/icons/card_images/Image (6).png'
+      imageUrl: 'assets/images/icons/card_images/newdelhi.jpg'
     },
     {
       id: '9',
       name: 'Brihadeeswarar Temple',
       location: 'Thanjavur',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl: 'assets/images/icons/card_images/Image (7).png'
+      imageUrl: 'assets/images/icons/card_images/thanjavur.jpg'
     },
     {
       id: '10',
       name: 'Tirupati Tirumala',
       location: 'Tirumala',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla volutpat lorem ac erat maximus vulputate',
-      imageUrl: 'assets/images/icons/card_images/Image (8).png'
+      imageUrl: 'assets/images/icons/card_images/tirumali.jpg'
     }
   ];
 
